3691f8f58eb971003bf8a3b4d2b83687914a6e74
Rushy Panchal <panchal.rushy@gmail.com>
2015-01-31 21:41:20 -0500

    More LISP operations

--- pylisp.py
+++ pylisp.py
@@ -2,7 +2,6 @@
 # pylisp.py
 # A basic LISP interpreter, created in Python
 
-import copy
 import re
 
 lispExtract = re.compile("\(? ?((?:\w)+|\([\w (]+\))").findall
@@ -10,6 +9,7 @@ lispExtract = re.compile("\(? ?((?:\w)+|\([\w (]+\))").findall
 class Expression(object):
 	'''A LISP Expression'''
 	def __init__(self, expression):
+		self.expression = expression
 		if isinstance(expression, list):
 			self.elements = expression
 		else:
@@ -17,14 +17,25 @@ class Expression(object):
 
 	def evaluate(self, **values):
 		'''Evaluates the LISP expression with the given values'''
-		return Interpreter.evaluate(copy.deepcopy(self.elements), **values)
+		# return Interpreter.evaluate(copy.deepcopy(self.elements), **values)
+		return Interpreter.interpret(self.expression, **values)
 
 class Interpreter(object):
 	'''Contains basic LISP interpretation methods'''
 	@classmethod
-	def interpret(cls, expression):
+	def interpret(cls, expression, **values):
 		'''Interprets a given LISP expression and returns a Lisp object'''
-		return Expression(cls.parse(expression))
+		return cls.evaluate(cls.parse(cls.compile(expression, **values)))
+
+	@classmethod
+	def compile(cls, expression, **values):
+		'''Compiles an expression with given values'''
+		spaced = " {0} ".format
+		bracketed = " {0})".format
+		compiledExpr = expression
+		for var, value in values.items():
+			compiledExpr = compiledExpr.replace(spaced(var), spaced(value)).replace(bracketed(var), bracketed(value))
+		return compiledExpr
 
 	@classmethod
 	def parse(cls, expression):
@@ -34,16 +45,17 @@ class Interpreter(object):
 		return map(recursiveExtract, raw_elements)
 
 	@classmethod
-	def evaluate(cls, expressions, original = None, index = 0, **values):
+	def evaluate(cls, expressions, original = None, index = 0):
 		'''Evaluate the given expressions recursively'''
 		if not original:
 			original = expressions
 		for itemIndex, expr in enumerate(expressions):
 			if isinstance(expr, list):
-				cls.evaluate(expr, expressions, itemIndex, **values)
+				cls.evaluate(expr, expressions, itemIndex)
 		op = expressions[0]
 		if cls.isOperator(op):
-			opValue = OPERATIONS[expressions[0]](expressions[1:], **values)
+			opArgs = map(float, expressions[1:])
+			opValue = OPERATIONS[expressions[0]](*opArgs)
 		else:
 			opValue = expressions
 		original[index] = opValue
@@ -54,25 +66,26 @@ class Interpreter(object):
 		'''Returns whether or not op is a valid operator'''
 		return op in VALID_OPERATIONS
 
-def operation(function):
-	'''Wrapper function to allow runtime-evaluation of LISP operations'''
-	def wrapper(arguments, **values):
-		arguments = map(lambda x: values[x] if isinstance(x, str) else x, arguments)
-		return function(*arguments)
-	return wrapper
+	@classmethod
+	def register(cls, op, function):
+		'''Registers a new operator'''
+		OPERATIONS[op] = function
 
 OPERATIONS = {
-	"ADD": 		operation(lambda *args: sum(args)),
-	"SUB": 		operation(lambda a, b: a -b),
-	"MULT": 	operation(lambda *args: reduce(lambda a, b: a * b, args)),
-	"DIV": 		operation(lambda a, b: float(a) / b),
-	"SQUARE":	operation(lambda a: a**2),
-	"EXP": 		operation(lambda a, n: a**n),
-	"EQ": 		operation(lambda a, b: a == b),
-	"POS": 		operation(lambda a: a > 0),
-	"NEG": 		operation(lambda a: a < 0),
-	"MAX": 		operation(lambda *args: max(args)),
-	"MIN": 		operation(lambda *args: min(args)),
+	"ADD": 		lambda *args: sum(args),
+	"SUB": 		lambda a, b: a -b,
+	"MULT": 	lambda *args: reduce(lambda a, b: a * b, args),
+	"DIV": 		lambda a, b: float(a) / b,
+	"EXP": 		lambda a, n: a**n,
+	"SQUARE":	lambda a: a**2,
+	"SQRT": 	lambda a: a**0.5,
+	"CUBE": 	lambda a: a**3,
+	"CUBERT":	lambda a: a**(1.0 / 3),
+	"EQ": 		lambda a, b: a == b,
+	"POS": 		lambda a: a > 0,
+	"NEG": 		lambda a: a < 0,
+	"MAX": 		lambda *args: max(args),
+	"MIN": 		lambda *args: min(args),
 	}
 
 VALID_OPERATIONS = OPERATIONS.keys()
